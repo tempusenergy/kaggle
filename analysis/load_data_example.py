d7b01c87592b77dda09ca1ff84aaf7ef02f954cb
anguschadney <angus.chadney@gmail.com>
2016-07-12 14:07:21 +0100

    no longer need to set path

--- analysis/load_data_example.py
+++ analysis/load_data_example.py
@@ -4,18 +4,13 @@
 # ## Loading Data...
 # This notebook is an example showing how to load the data with a custom loading class
 
-# In[5]:
-
-# Add repo directory to path so that we can do the relative import below
-# Probably a better way to do this!
-import sys
-sys.path.append('/home/gus/Repos/kaggle')
+# In[1]:
 
 # Import GrupoBimboData
 from tools.load_data import GrupoBimboData
 
 
-# In[10]:
+# In[2]:
 
 # Initialise with file containing downloaded datafiles (from https://www.kaggle.com/c/grupo-bimbo-inventory-demand/data)
 # NOTE: no need to unzip files
@@ -23,33 +18,33 @@ gp = GrupoBimboData('/home/gus/Repos/kaggle/data')
 print(gp.data)
 
 
-# In[12]:
+# In[3]:
 
 # Load data
 gp.load_all()
 
 
-# In[13]:
+# In[4]:
 
 gp.data['cliente_tabla'].head()
 
 
-# In[14]:
+# In[5]:
 
 gp.data['producto_tabla'].head()
 
 
-# In[15]:
+# In[6]:
 
 gp.data['town_state'].head()
 
 
-# In[16]:
+# In[7]:
 
 gp.data['test'].head()
 
 
-# In[17]:
+# In[8]:
 
 # NOTE: By default train is limited to 1000 rows
 gp.data['train'].head()
@@ -65,7 +60,7 @@ gp.data['train'].head()
 # 
 # To override the default for 'train' or to pass through additional params, use `load_arg_override` in `load_all()`, e.g:
 
-# In[18]:
+# In[9]:
 
 extra_args = data_load_args = {                                                          
     'train': {'nrows': 2000},
@@ -74,12 +69,12 @@ extra_args = data_load_args = {
 gp.load_all(load_arg_override = data_load_args)
 
 
-# In[19]:
+# In[10]:
 
 gp.data['train'].shape
 
 
-# In[20]:
+# In[11]:
 
 gp.data['cliente_tabla'].head()
 
