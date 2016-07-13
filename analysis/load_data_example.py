569175c60482904d8d2e4f071a356d2f851c5111
anguschadney <angus.chadney@gmail.com>
2016-07-13 09:40:09 +0100

    updated with changes to load_data api

--- analysis/load_data_example.py
+++ analysis/load_data_example.py
@@ -15,68 +15,40 @@ from tools.load_data import GrupoBimboData
 # Initialise with file containing downloaded datafiles (from https://www.kaggle.com/c/grupo-bimbo-inventory-demand/data)
 # NOTE: no need to unzip files
 gp = GrupoBimboData('/home/gus/Repos/kaggle/data')
-print(gp.data)
+print(dir(gp))
 
 
 # In[3]:
 
 # Load data
-gp.load_all()
+gp.load_data('cliente_tabla')
+gp.cliente_tabla.head()
 
 
 # In[4]:
 
-gp.data['cliente_tabla'].head()
+gp.load_data('producto_tabla')
+gp.producto_tabla.head()
 
 
 # In[5]:
 
-gp.data['producto_tabla'].head()
+gp.load_data('town_state')
+gp.town_state.head()
 
 
 # In[6]:
 
-gp.data['town_state'].head()
+gp.load_data('test')
+gp.test.head()
 
 
 # In[7]:
 
-gp.data['test'].head()
-
-
-# In[8]:
-
-# NOTE: By default train is limited to 1000 rows
-gp.data['train'].head()
-
-
-# ### Param Override
-# Currently, the only extra params that get sent to `pandas.read_csv` are:
-# ```python
-# data_load_args = {                                                          
-#     'train': {'nrows': 1000},                                               
-# }  
-# ```
-# 
-# To override the default for 'train' or to pass through additional params, use `load_arg_override` in `load_all()`, e.g:
-
-# In[9]:
-
-extra_args = data_load_args = {                                                          
-    'train': {'nrows': 2000},
-    'cliente_tabla': {'index_col': 0},
-}
-gp.load_all(load_arg_override = data_load_args)
-
-
-# In[10]:
-
-gp.data['train'].shape
-
-
-# In[11]:
-
-gp.data['cliente_tabla'].head()
+# And with additional args...
+gp.load_data('train', nrows=1000)
+print(gp.train.shape)
+gp.train.head()
 
 
 # In[ ]:
